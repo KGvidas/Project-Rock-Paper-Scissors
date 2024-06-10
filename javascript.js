a8c73dc54dd19f2154bc14653543adfb72e8e942
KG <122453305+KGvidas@users.noreply.github.com>
2024-06-10 15:38:44 +0300

    Delete global variables from func

--- javascript.js
+++ javascript.js
@@ -1,23 +1,22 @@
-// Global variables
-let computerChoice;
-let humanChoice;
-
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-      computerChoice = "rock"
-      return computerChoice;
+      return "rock"
     } else if (randomNumber === 1) {
-      computerChoice = "paper";
-      return computerChoice;
+      return "paper";
     } else {
-      computerChoice = "scissors";
-      return computerChoice;
+      return "scissors";
     }
   }
   
   function getHumanChoice(){
     let humanChoice = prompt("Choose your weapon: rock, paper or scissors?").toLocaleLowerCase();
-    console.log(humanChoice)
+    return humanChoice;
   }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
