8c4ae362fb0930108341f6082b288486595f6daf
KG <122453305+KGvidas@users.noreply.github.com>
2024-06-11 15:14:31 +0300

    Add updateScore func

--- javascript.js
+++ javascript.js
@@ -17,24 +17,25 @@ function getComputerChoice() {
   }
 
 
-
+function updateScore (humanScore, computerScore){
+  console.log("Your score is: " + humanScore);
+  console.log("Computer score is: " + computerScore);
+}
 
 function playRound(humanSelection, computerSelection,humanScore, computerScore){
 if (humanSelection === "rock" && computerSelection === "scissors" ||
     humanSelection === "paper" && computerSelection === "rock"     ||
     humanSelection === "scissors" && computerSelection === "paper") {
-      
       ++humanScore;
       console.log("You won! " + "your choice was " + humanSelection + " and computer's choice was " + computerSelection)
-      console.log(humanScore);
-      console.log(computerScore);
-} else if (humanSelection === computerSelection) {
+      updateScore (humanScore, computerScore)
+    } else if (humanSelection === computerSelection) {
   console.log("It's a tie! " + "your choice was " + humanSelection + " and computer's choice was " + computerSelection)
+  updateScore (humanScore, computerScore)
 } else {
   ++computerScore;
     console.log("You lost! Your choice was " + humanSelection + " and computer's choice was " + computerSelection);
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
+    updateScore (humanScore, computerScore);
 }};
 
 
@@ -48,3 +49,7 @@ playRound(humanSelection, computerSelection, humanScore, computerScore);
 }
 
 playGame();
+
+// Re-work your previous functions or create more helper functions 
+// if necessary. Specifically, you may want to change the return values to something more useful.
+// If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
